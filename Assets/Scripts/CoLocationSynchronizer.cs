846c53d3c12fca5e8044154c3685b7324d8337ea
Tallido <giancarlo.danese@mail.polimi.it>
2022-05-03 17:29:54 +0200

    added pose

---
+++ Assets/Scripts/CoLocationSynchronizer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Oculus.Interaction;
+using UnityEngine;
+
+public class CoLocationSynchronizer : MonoBehaviour
+{
+
+  public void Synchronize()
+  {
+    Debug.Log("Synchronizing");
+  }
+  
+}
