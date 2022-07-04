10455dbe55d6a4221abca8c3693ce261a7fba098
Tallido <giancarlo.danese@mail.polimi.it>
2022-07-04 17:03:04 +0200

    WE ARE DONEEEEE

--- Assets/Scripts/CoLocationSynchronizer.cs
+++ Assets/Scripts/CoLocationSynchronizer.cs
@@ -7,7 +7,6 @@ using UnityEngine.InputSystem;
 public class CoLocationSynchronizer : MonoBehaviourPunCallbacks, XRIDefaultInputActions.ISynchronizeActions
 {
     private XRIDefaultInputActions _defaultInputActions;
-
     private const byte SendIDForSync = 1;
     private const byte SendPositionForCoLocation = 2;
     private const byte ResetID = 3;
@@ -44,27 +43,31 @@ public class CoLocationSynchronizer : MonoBehaviourPunCallbacks, XRIDefaultInput
         {
             var data = (object[]) photonEvent.CustomData;
    
+            //Receiving Mean Hand information from the master client
             var otherMeanHandPosition = (Vector3)data[0];
             var otherMeanHandRotation = (Quaternion)data[1];
-            
-            var myRightHand = GameObject.Find("RightHandAnchor");
-            var myLeftHand = GameObject.Find("LeftHandAnchor");
-            
-            var myMeanHandPosition = Vector3.Lerp(myLeftHand.transform.position, myRightHand.transform.position, 0.5f);
-            var myMeanHandRotation = Quaternion.Lerp(myLeftHand.transform.rotation, myRightHand.transform.rotation, 0.5f);
 
+            //Finding Mean Hand information of the player to be positioned (ourselves)
+            var myMeanHandPosition = Vector3.Lerp(GameObject.Find("LeftHandAnchor").transform.position, GameObject.Find("RightHandAnchor").transform.position, 0.5f);
+            var myMeanHandRotation = Quaternion.Lerp(GameObject.Find("LeftHandAnchor").transform.rotation, GameObject.Find("RightHandAnchor").transform.rotation, 0.5f);
+
+            //CALCULATING ROTATION
             var deltaRotation = otherMeanHandRotation.eulerAngles.y - myMeanHandRotation.eulerAngles.y;
 
+            //CALCULATING POSITION
             var deltaPosition = otherMeanHandPosition - myMeanHandPosition;
             deltaPosition.y = 0;
             
+            //APPLYING ROTATION AND POSITION
             GameObject.Find("OVRCameraRig").gameObject.transform.RotateAround(myMeanHandPosition, Vector3.up, 180 + deltaRotation);
             GameObject.Find("OVRCameraRig").gameObject.transform.position += deltaPosition;
             
+            //Resetting ID of player to be positioned
             RaiseEventOptions raiseEventOptions = new RaiseEventOptions {Receivers = ReceiverGroup.All};
             PhotonNetwork.RaiseEvent(ResetID, 0, raiseEventOptions, SendOptions.SendReliable);
         }
 
+        //Event triggered at the end of the above event
         if (photonEvent.Code == ResetID && gameObject.GetPhotonView().IsMine)
         {
             _idOfPlayerToBePositioned = (int)photonEvent.CustomData;
@@ -92,14 +95,9 @@ public class CoLocationSynchronizer : MonoBehaviourPunCallbacks, XRIDefaultInput
         //Sending master client origin system information to the player who wants to be positioned
         if (PhotonNetwork.IsMasterClient && gameObject.GetPhotonView().IsMine)
         {
-            var tempRightHand = GameObject.Find("RightHandAnchor");
-            var tempLeftHand = GameObject.Find("LeftHandAnchor");
+            var meanHandPosition = Vector3.Lerp(GameObject.Find("LeftHandAnchor").transform.position, GameObject.Find("RightHandAnchor").transform.position, 0.5f);
+            var meanHandRotation = Quaternion.Lerp(GameObject.Find("LeftHandAnchor").transform.rotation, GameObject.Find("RightHandAnchor").transform.rotation, 0.5f);
 
-            var meanHandPosition = Vector3.Lerp(tempLeftHand.transform.position, tempRightHand.transform.position, 0.5f);
-            var meanHandRotation = Quaternion.Lerp(tempLeftHand.transform.rotation, tempRightHand.transform.rotation, 0.5f);
-            
-            Debug.Log("MY MEAN ROTATION IS: " + meanHandRotation.eulerAngles.y);
-            
             object[] posInfoToSend = {meanHandPosition, meanHandRotation};
 
             RaiseEventOptions raiseEventOptions = new RaiseEventOptions {Receivers = ReceiverGroup.Others};
