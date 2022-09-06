ba3538293049b3d54a1bdd00bd3d5c2a78aee6bf
Tallido <giancarlo.danese@mail.polimi.it>
2022-09-06 21:22:29 +0200

    Revert "nearly finished"
    This reverts commit 984495a1fec0b226b161b0577a62dfaa36f080ad.

--- Assets/Oculus/VR/Scripts/Util/OVRGrabber.cs
+++ Assets/Oculus/VR/Scripts/Util/OVRGrabber.cs
@@ -340,7 +340,7 @@ public class OVRGrabber : MonoBehaviour
         }
     }
 
-    virtual protected void GrabEnd()
+    protected void GrabEnd()
     {
         if (m_grabbedObj != null)
         {
