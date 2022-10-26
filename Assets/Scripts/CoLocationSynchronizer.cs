e1e23eaaca8d3dbbfc1312af88285ab0a1f0ebf6
Tallido <giancarlo.danese@mail.polimi.it>
2022-10-26 11:13:47 +0200

    removed extra rotation

--- Assets/Scripts/CoLocationSynchronizer.cs
+++ Assets/Scripts/CoLocationSynchronizer.cs
@@ -59,7 +59,7 @@ public class CoLocationSynchronizer : MonoBehaviourPunCallbacks, XRIDefaultInput
                 var meanHandRotation = Quaternion.Lerp(GameObject.Find("LeftHandAnchor").transform.rotation, GameObject.Find("RightHandAnchor").transform.rotation, 0.5f);
                 meanHandRotation = Quaternion.Euler(0 , meanHandRotation.eulerAngles.y, 0);
                 
-                object[] posInfoToSend = {meanHandPosition, meanHandRotation, Vector3.ProjectOnPlane(gameObject.GetComponent<NetworkPlayer>().head.forward, Vector3.up)};
+                object[] posInfoToSend = {meanHandPosition, meanHandRotation};
 
                 RaiseEventOptions raiseEventOptions = new RaiseEventOptions {Receivers = ReceiverGroup.Others};
                 PhotonNetwork.RaiseEvent(SendPositionForCoLocation, posInfoToSend, raiseEventOptions, SendOptions.SendReliable);
@@ -85,10 +85,6 @@ public class CoLocationSynchronizer : MonoBehaviourPunCallbacks, XRIDefaultInput
             //Receiving Mean Hand information and Forward Vector information from the master client
             var otherMeanHandPosition = (Vector3)data[0];
             var otherMeanHandRotation = (Quaternion)data[1];
-            
-            /*
-             * var otherForwardVector = (Vector3)data[2];
-             */
 
             //Finding Mean Hand information of the player to be positioned (ourselves)
             var myMeanHandPosition = Vector3.Lerp(GameObject.Find("LeftHandAnchor").transform.position, GameObject.Find("RightHandAnchor").transform.position, 0.5f);
@@ -105,18 +101,7 @@ public class CoLocationSynchronizer : MonoBehaviourPunCallbacks, XRIDefaultInput
             //APPLYING ROTATION AND POSITION
             GameObject.Find("OVRCameraRig").gameObject.transform.RotateAround(myMeanHandPosition, Vector3.up, 180 + deltaRotation);
             GameObject.Find("OVRCameraRig").gameObject.transform.position += deltaPosition;
-            
-            /*
-            //EVENTUAL EXTRA ROTATION
-            if(Vector3.Dot(otherForwardVector, Vector3.ProjectOnPlane(gameObject.GetComponent<NetworkPlayer>().head.forward, Vector3.up)) > 0)
-            {
-                RaiseEventOptions eventOptions = new RaiseEventOptions {Receivers = ReceiverGroup.All};
-                PhotonNetwork.RaiseEvent(DebugCode, 0, eventOptions, SendOptions.SendReliable);
-                
-                GameObject.Find("OVRCameraRig").gameObject.transform.RotateAround(myMeanHandPosition, Vector3.up, 180);
-            }
-            */
-            
+
             //Resetting ID of player to be positioned
             RaiseEventOptions raiseEventOptions = new RaiseEventOptions {Receivers = ReceiverGroup.All};
             PhotonNetwork.RaiseEvent(ResetID, 0, raiseEventOptions, SendOptions.SendReliable);
@@ -129,13 +114,6 @@ public class CoLocationSynchronizer : MonoBehaviourPunCallbacks, XRIDefaultInput
             gameObject.GetComponent<NetworkPlayer>().SetStateHasChanged(true);
             Debug.Log("Co-Location ended");
         }
-        
-        /*
-        if (photonEvent.Code == DebugCode && gameObject.GetPhotonView().IsMine)
-        {
-            Debug.Log("EXTRA ROTATION DONE!!");
-        }
-        */
     }
 
     public int GetIdOfPlayerToBePositioned()
